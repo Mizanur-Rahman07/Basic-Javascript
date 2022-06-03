a3831ee16c098e7b960acae356dd7c81007ce0fb
Mizanur-Rahman07 <mzrahman075@gmail.com>
2022-06-03 18:51:36 +0600

    change

--- 5-javascript_problems/random.js
+++ 5-javascript_problems/random.js
@@ -1,21 +1,32 @@
-var num = 2.9999999;
-var result = Math.floor(num);
-console.log(result);
+// // math.floor use korle purno number ar por jai thak seta dakhabe na .seta purnp number ai asbe
+// var num = 2.9999999;
+// var result = Math.floor(num);
+// console.log(result);
+// // ----------------------
 
-var num = 2.33333;
-var result2 = Math.ceil(num);
-console.log(result2);
+// // math.ceil use korle purno number ar porer dosomik number gulo k 1 hisab a dhorbe
+// var num = 2.33333;
+// var result2 = Math.ceil(num);
+// console.log(result2);
+// // -----------------------
 
-var num = 2.4444;
-var result3 = Math.round(num);
-console.log(result3);
+// // math. round use korle purno number ar sathe j dosomik songkha thakbe seta half ar bashi hole 2 theka 3 hoye jabe ,,, 
+// // r half ar kom hole seta purno songkhai thakbe .dosomik ar value thakbena / ex 2
+// var num = 2.4444;
+// var result3 = Math.round(num);
+// console.log(result3);
+// ---------------------------
 
-var num = 2.888;
-var dice = Math.random()*10;
-console.log(dice);
+//------------------- lotary kora jay
+var num = 4;
+var dice = Math.random()*4;
+var output = Math.round(dice);
+console.log(output);
+// -------------
 
-for (var i = 0; i<10; i++){
-    var randomNum = Math.random() * 6;
+// r o valo upay lotary ar ----------
+for (var i = 1; i<=3; i++){
+    var randomNum = Math.random() * 4;
     var output = Math.round(randomNum);
     console.log(output);
 }
